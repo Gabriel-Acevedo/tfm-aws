7fb7e7c9d7aac655b0389d6ba42e12ac9ce8ceae
Gabriel Gordo <gabriel.gordo@acevedoapps.com>
2020-07-04 12:34:04 +0200

    MODIFICATION: EXecuted sam build locally.

---
+++ .aws-sam/build/customerFunction/dbManager.js
@@ -0,0 +1,53 @@
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+const REGION = "us-east-1";
+
+AWS.config.update({
+    endpoint: "https://dynamodb." + REGION + ".amazonaws.com"
+});
+
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const table = 'customers';
+
+
+const getAllCustomers = () => {
+    const params = {
+        TableName: table
+    };
+
+    return docClient.scan(params).promise();
+};
+
+
+const getCustomer = (customerid) => {
+    const params = {
+        TableName: table,        
+        KeyConditionExpression: "customerid = :customerid",
+        ExpressionAttributeValues: {
+            ":customerid": customerid
+        },
+    };
+    return docClient.query(params).promise();
+};
+
+
+const addCustomer = (customer) => {
+    const params = {
+        TableName: table,
+        Item: {
+            "customerid": uuid.v1(),
+            "name": customer.name,
+            "age": customer.age
+        }
+    };
+
+    return docClient.put(params).promise();
+};
+
+
+module.exports = {
+    getAllCustomers,
+    getCustomer, 
+    addCustomer
+};
