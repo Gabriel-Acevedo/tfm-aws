15180e8e8822ceaa9eebac54e3dedf966bf3aa19
Gabriel Gordo <gabriel.gordo@acevedoapps.com>
2020-07-18 17:07:25 +0200

    FEATURE: Added Product functions into index.js file.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const dbManager = require('./dbManager');
+const dbProductManager = require('./dbProductManager');
 
 
 exports.customerHandler = (event, context, callback) => {
@@ -21,6 +22,24 @@ exports.customerHandler = (event, context, callback) => {
 };
 
 
+exports.productHandler = (event, context, callback) => {
+    switch (event.httpMethod) {     
+        case 'GET':
+            if (event.pathParameters && event.pathParameters.productid){
+                getProduct(event.pathParameters.productid, callback);
+            }else{
+                getAllProducts(callback);         
+            } 
+            break;
+        case 'POST':
+            addProduct(event.body, callback);
+            break;
+        default:
+            sendResponse(400, `Unsupported method ${event.httpMethod}`, callback);
+    }
+};
+
+//Customer Functions
 const getAllCustomers = (callback) => {
     dbManager.getAllCustomers()
     .then((res) => {
@@ -57,6 +76,48 @@ const addCustomer = (data, callback) => {
         sendResponse(200, err, callback);
     });
 };
+//End Customer Functions
+
+
+//Product Functions
+const getAllProducts = (callback) => {
+    dbProductManager.getAllProducts()
+    .then((res) => {
+        sendResponse(200, res, callback);
+    })
+    .catch((err) => {
+        console.log(err);
+        sendResponse(200, err, callback);
+    });
+};
+
+
+const getProduct = (customerid, callback) => {
+    dbProductManager.getProduct(customerid)
+    .then((res) => {
+        sendResponse(200, res, callback);
+    })
+    .catch((err) => {
+        console.log(err);
+        sendResponse(200, err, callback);
+    });
+};
+
+
+const addProduct = (data, callback) => {
+    data = JSON.parse(data);
+
+    dbProductManager.addProduct(data)
+    .then((res) => {
+        sendResponse(200, res, callback);
+    })
+    .catch((err) => {
+        console.log(err);
+        sendResponse(200, err, callback);
+    });
+};
+//End Product Functions
+
 
 
 const sendResponse = (statusCode, message, callback) => {
