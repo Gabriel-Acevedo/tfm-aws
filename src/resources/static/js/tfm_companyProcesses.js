df39b1abbd5a9be2a30036344964f7084498797b
Gabriel Gordo <gabriel.gordo@acevedoapps.com>
2020-08-18 21:35:59 +0200

    FEATURE: Added tfm_companyProcesses.js file to static/js files.

---
+++ src/resources/static/js/tfm_companyProcesses.js
@@ -0,0 +1,28 @@
+var AWS_ENDPOINT = getAwsEndpoint();
+var receiveidCustomerId = localStorage["customerid"];
+
+var API_POST_COMPANY = AWS_ENDPOINT + "/api/" + receiveidCustomerId  + "/company";
+
+async function addCompany(){
+   return await postCompany();
+};
+
+function postCompany(){
+
+	$.ajax({
+	    	type: 'POST',
+			url: API_POST_COMPANY ,
+		    data: JSON.stringify({
+                                    "vatregnumber": $('#vatregnumber').val(),
+                                    "name": $('#name').val(),
+                                    "country": $('#country').val(),
+                                    "industry": $('#industry').val()
+                                }),
+					
+			succes: function(response){
+               console.log("Company id: " + response);
+			   location.reload(true);
+			}
+	});
+
+};	
