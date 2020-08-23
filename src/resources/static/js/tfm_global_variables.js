6edd6ee427a2e4669ff234b4ba957d1ae380bb8f
Gabriel Gordo <gabriel.gordo@acevedoapps.com>
2020-08-23 15:31:02 +0200

    MODIFIED: Added some global variable into tfm_global_variable file

--- src/resources/static/js/tfm_global_variables.js
+++ src/resources/static/js/tfm_global_variables.js
@@ -1,6 +1,38 @@
 //AWS:
-const awsEndpoint = "https://yh3fkv6r01.execute-api.us-east-1.amazonaws.com/Prod";
+const awsEndpoint = "https://e69r1txiva.execute-api.us-east-1.amazonaws.com/Prod";
+
 
 function getAwsEndpoint() {
 	return awsEndpoint;
+};
+
+
+function setCustomerId(p_customerId){
+	sessionStorage.setItem('CUSTOMERID', p_customerId);
+};
+
+
+function getCustomerId(){
+	var customerid = sessionStorage.getItem('CUSTOMERID');
+	return customerid;
+};
+
+
+
+function setCompanyId(p_companyId){
+	sessionStorage.setItem('COMPANYID', p_companyId);
+};
+function getCompanyId(){
+	var companyid = sessionStorage.getItem('COMPANYID');
+	return companyid;
+};
+
+
+
+function setBudgetId(p_budgetId){
+	sessionStorage.setItem('BUDGETID', p_budgetId);
+};
+function getBudgetId(){
+	var budgetid = sessionStorage.getItem('BUDGETID');
+	return budgetid;
 };
