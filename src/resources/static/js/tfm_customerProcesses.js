e6c1209cd140fc59f37deb4e13d3f5ecfd5230b8
Gabriel Gordo <gabriel.gordo@acevedoapps.com>
2020-08-26 19:34:10 +0200

    MODIFIED: Refactored code of tfm_customerProcesses file.

--- src/resources/static/js/tfm_customerProcesses.js
+++ src/resources/static/js/tfm_customerProcesses.js
@@ -1,26 +1,37 @@
 var AWS_ENDPOINT = getAwsEndpoint();
 var API_POST_CUSTOMER = AWS_ENDPOINT + "/api/customer";
 
-setSessionStorageId('CUSTOMERID', '');
-setSessionStorageId('COMPANYID', '');
-setSessionStorageId('BUDGETID', '');
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
 
-
-async function addCustomer(){
-   return await postCustomer();
-};
-
-function postCustomer(){
-    $.ajax({
-            type: 'POST',
+function changeUrl(){
+	
+	 if(!$('#name').val() || !$('#lastname').val() || !$('#email').val()){
+		alert ("Please enter a name, lastname and email!");
+	 }else{
+            if(validateEmail($('#email').val())){
+                $.ajax({
+                        type: 'POST',
 			url: API_POST_CUSTOMER,
 			data: JSON.stringify({
-								    "name": $('#name').val(),
-								    "lastname": $('#lastname').val(),
+									"name": $('#name').val(),
+									"lastname": $('#lastname').val(),
 									"email": $('#email').val()
-								}),			
+									}),			
 			success: function(response){
-				setSessionStorageId('CUSTOMERID', response);
+                             window.location.replace("https://acevedo.biz/drupal/en/awsCustomerCompany?custid="+response);
+			},
+			error: function(response){
+					alert ("Unexpected error ocurred calling Rest API!!");
 			}
-	});
-};		
+		});
+                
+            }else{
+                alert ("Please enter a valid email!");
+            }
+	 }
+		 return false;
+		 
+}
